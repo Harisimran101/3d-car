ca6457fda0ee938d71768f9f923e22c81e7ecfbb
itsmayowa1 <131627858+itsmayowa1@users.noreply.github.com>
2024-12-14 15:17:30 -0800

    Update top-menu.tsx

--- components/top-menu.tsx
+++ components/top-menu.tsx
@@ -47,7 +47,7 @@ export function TopMenu() {
         <MenubarMenu>
           <MenubarTrigger>Tour</MenubarTrigger>
           <MenubarContent>
-            <MenubarItem>Super Bowl LIX</MenubarItem>
+            <MenubarItem>Grand National Tour with SZA</MenubarItem>
             <MenubarSeparator />
             <MenubarItem inset>
               Reload <MenubarShortcut>⌘R</MenubarShortcut>
